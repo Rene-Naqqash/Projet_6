2b1390e7e922f416fd14affb5ca7d060491eb63e
Rene-Naqqash <165726455+Rene-Naqqash@users.noreply.github.com>
2024-10-07 00:41:22 +0200

    GET, POST middleware created. book model created.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Projet_6
+
+Expliquer ce que mon prof m'a expliquer quand j'ai fait le .env pour que je puisse mettre ça sur github 
+
+donc npm install 
+creer le fichiers .env
+puis ajouter la variable de mon lien vers ma Base de Données MongoDB dans le fichier .env etc etc 
--- app.js
+++ app.js
@@ -1,32 +1,61 @@
-const express = require('express')
-
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+require('dotenv').config()
 
+const Book = require('./models/book');
+
+// pour se connecter à MongoBD Atlas
+mongoose.connect(process.env.MONGO_URL,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+// pour pouvoir lire le contenue de la requette rentrante donc d'une requette  POST vers notre serveur
+app.use(express.json());
+
+// un middleWare pour gerer la securité CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+// le middleware pour gerer les requettes POST
+ app.post('/api/books', (req,res,next) => {
+  delete req.body._id;
+  const book = new Book({
+    ...req.body
   });
+  book.save()
+  .then(() => res.status(201).json({message: 'Objet enregistré !'}))
+  .catch( error => res.status(400).json({ error }));
+ });
 
-app.use((req, res, next) => {
-    console.log('Requête reçu avec succès!!');
-    next();
-})
+
+// le middleware pour gerer les requettes GET 
+app.get( '/api/books',(req, res, next) => {
+    Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));
+});
 
 app.use((req, res, next) => {
     res.status(202);
     next();
-})
+});
 
 app.use((req, res, next) => {
     res.json({ message : 'Votre Requête a bien été reçue! '})
     next();
-})
+});
 
 app.use((req, res) => {
     console.log('Reponse envoyée avec succès!')
-})
+});
 
 
 
