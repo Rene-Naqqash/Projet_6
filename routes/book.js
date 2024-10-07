cc731bc43a11a205d46b668dcee870799ca4b928
Rene-Naqqash <165726455+Rene-Naqqash@users.noreply.github.com>
2024-10-08 00:18:31 +0200

    multer installed

--- routes/book.js
+++ routes/book.js
@@ -4,9 +4,9 @@ const auth = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/',auth, bookCtrl.createBook);
-router.get( '/',auth, bookCtrl.getAllBooks);
+router.get( '/', bookCtrl.getAllBooks);
 router.put('/:id',auth, bookCtrl.modifyBook);
-router.get('/:id',auth, bookCtrl.getOneBook);
+router.get('/:id', bookCtrl.getOneBook);
 router.delete('/:id',auth, bookCtrl.deleteBook);
 
 
