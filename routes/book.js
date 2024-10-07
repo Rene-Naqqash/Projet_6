986c9336681948be6e9e80a157ab6d27ab645bb6
Rene-Naqqash <165726455+Rene-Naqqash@users.noreply.github.com>
2024-10-07 20:03:57 +0200

    mongoose-unique-validator installed

--- routes/book.js
+++ routes/book.js
@@ -4,10 +4,10 @@ const bookCtrl = require('../controllers/book');
 
 
 router.post('/', bookCtrl.createBook);
+router.get( '/', bookCtrl.getAllBooks);
 router.put('/:id', bookCtrl.modifyBook);
-router.delete('/:id', bookCtrl.deleteBook);
-router.get( '/' + '', bookCtrl.getAllBooks);
 router.get('/:id', bookCtrl.getOneBook);
+router.delete('/:id', bookCtrl.deleteBook);
 
 
 module.exports = router;
