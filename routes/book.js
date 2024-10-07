e81d915b0c63cccf72bdcd643f61dfcd94f75563
Rene-Naqqash <165726455+Rene-Naqqash@users.noreply.github.com>
2024-10-07 23:54:25 +0200

    Typo fixed and middleware auth added

--- routes/book.js
+++ routes/book.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const router = express.Router();
 const bookCtrl = require('../controllers/book');
+const auth = require('../middleware/auth');
+const router = express.Router();
 
-
-router.post('/', bookCtrl.createBook);
-router.get( '/', bookCtrl.getAllBooks);
-router.put('/:id', bookCtrl.modifyBook);
-router.get('/:id', bookCtrl.getOneBook);
-router.delete('/:id', bookCtrl.deleteBook);
+router.post('/',auth, bookCtrl.createBook);
+router.get( '/',auth, bookCtrl.getAllBooks);
+router.put('/:id',auth, bookCtrl.modifyBook);
+router.get('/:id',auth, bookCtrl.getOneBook);
+router.delete('/:id',auth, bookCtrl.deleteBook);
 
 
 module.exports = router;
