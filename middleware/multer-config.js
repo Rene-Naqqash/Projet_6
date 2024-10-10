2bcf779711693dd6d90ff5febe0bb77c3b821167
Rene-Naqqash <renenakkash1@gmail.com>
2024-10-10 05:09:31 +0200

    Middleware modif

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -2,13 +2,6 @@ const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
 
-
-const MIME_TYPES = {
-    'image/jpg': 'jpg',
-    'image/jpeg': 'jpg',
-    'image/png': 'png'
-}; 
-
 // la fonction qui créer le dossier images si nécessaire donc s'il est pas deja là
 const ensureImagesDirectoryExists = () => {
     const imagesDir = path.join(__dirname, '../images'); // Chemin vers le dossier images
@@ -20,16 +13,24 @@ const ensureImagesDirectoryExists = () => {
 // lancer la fonction de verif
 ensureImagesDirectoryExists();
 
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png'
+}; 
+
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
         callback(null, 'images')
     },
     filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
+        const name = file.originalname.split('.').slice(0,-1).join('.').split(' ').join('_');
         const extension = MIME_TYPES[file.mimetype];
-        callback(null,name + Date.now() + '.' + extension);
-
+        callback(null,name + '_'+ Date.now()+ 'sec' + '.' + extension);
+       
     }
 });
+
+
 
 module.exports = multer({ storage: storage }).single('image');
--- middleware/sharp-config.js
+++ middleware/sharp-config.js
@@ -1,21 +1,26 @@
+const fs = require('fs').promises; // Utilisation des promesses de fs
 const sharp = require('sharp');
-const fs = require('fs');
-const path = require('path');
 
 module.exports = async function processImage(filePath) {
     try {
         const outputFilePath = filePath.replace(/\.[^.]+$/, '.webp'); // Remplace l'extension par .webp
+
         await sharp(filePath)
             .webp({ quality: 20 })
             .toFile(outputFilePath); // Enregistre la version compressée
 
-        // Supprime le fichier original
-        fs.unlinkSync(filePath);
+        // // Supprime le fichier original de manière asynchrone
+        // setTimeout(async () => {
+        //     try {
+        //         await fs.unlink(filePath);
+        //     } catch (error) {
+        //         console.error("Erreur lors de la suppression du fichier avec fs.promises.unlink:", error);
+        //     }
+        // }, 100); // Délai de 100 ms avant la suppression
 
-        // Retourne le chemin de l'image compressée
-        return outputFilePath;
+        return outputFilePath; // Retourne le chemin de l'image compressée
     } catch (error) {
-        console.error('Erreur lors du traitement de l\'image avec sharp:', error);
+        console.error("Erreur lors du traitement de l'image avec sharp:", error);
         throw error;
     }
 };
